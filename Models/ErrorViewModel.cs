2cc8c917fbdc21728387733f4443b72e6ec70f2c
Sara Marcely <saradomacario@gmail.com>
2025-06-23 20:43:47 -0300

    EComunidadeMVC - Projeto Final

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EComunidadeMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/EventoViewModel.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EComunidadeMVC.Models
+{
+    public class EventoViewModel
+    {
+        public int IdEvento { get; set; }
+
+        public string? TituloEvento  { get; set; }
+
+        public string? DescricaoEvento { get; set; }
+
+        public string? DataEvento { get; set; }
+
+        public string? HoraEvento { get; set;}
+
+        public int QtVoluntarios { get; set; }
+
+        public int DuracaoEvento { get; set; }
+
+        public int PontuacaoEvento { get; set; }
+
+        public string? LocalEvento { get; set; }
+
+        public string? Situacao { get; set;}
+    }
+}
